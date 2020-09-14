754b7d149ad49f1e9cb9b71fda099b8cd3a36ddc
fatim <fatim@gmail.com>
2020-09-14 22:11:09 +0100

    update of classe WhatsWrong1

--- app/WhatsWrong1.scala
+++ app/WhatsWrong1.scala
@@ -4,14 +4,20 @@ object WhatsWrong1 {
 
   trait Interface {
     val city: String
-    val support: String = s"Ici c'est $city !"
+    val support: String
   }
 
   case object Supporter extends Interface {
 
     override val city = "Paris"
+    override val support = s"Ici c'est $city !"
+
   }
 
-  Supporter.city //What does this print ?
-  Supporter.support //What does this print and why ? How to fix it ?
+  Supporter.city //What does this print ? It print Paris
+  Supporter.support //What does this print and why ? How to fix it ? It print " Ici c'est null !"
+  // we must have the declaration of supporter in the interface and its definition in the object Supporter
+  // as for the city as i modified in the code
+
+
 }
